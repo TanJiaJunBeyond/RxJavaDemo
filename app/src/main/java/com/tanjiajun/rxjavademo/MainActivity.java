4c3e3a764ece9c4700880a87bfe19ec289d8b005
TanJiaJun <1120571286@qq.com>
2019-10-29 23:11:51 +0800

    优化代码。

--- app/src/main/java/com/tanjiajun/rxjavademo/MainActivity.java
+++ app/src/main/java/com/tanjiajun/rxjavademo/MainActivity.java
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Observable.create((ObservableOnSubscribe<String>) emitter -> {
             emitter.onNext("Tan");
             emitter.onNext("Jia");
